9272a80dc633654e617a87639fe9dce1e2ed7ac2
Vladimir <dr.frisbi@gmail.com>
2022-12-21 20:58:02 +0700

    решение через массив

--- 009_IntroArray/Program.cs
+++ 009_IntroArray/Program.cs
@@ -5,16 +5,12 @@
     if(arg3 > result) result = arg3;
     return result;
 }
+ //             0    1   2   3   4   5   6   7   8
+int[] array = {11, 211, 31, 41, 15, 61, 17, 18, 19};
 
-
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
-
-int max = Max(
-    Max(a1, b1, c1),
-    Max(a2, b2, c2),
-    Max(a3, b3, c3));
-
-Console.WriteLine(max);
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
